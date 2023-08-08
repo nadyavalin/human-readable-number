e47ecd017b38fb0e3213e7672a0aadb4808466b8
nadyavalin <nadejda.sergeevna.tkachuk@gmial.com>
2023-08-08 11:14:51 +0300

    feat: do task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    const digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+    const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    
+    if (number === 0) {
+        return "zero";
+    } else if (number <= 10) {
+        return digits[number];
+    } else if (number <= 20) {
+        return teens[number - 10];
+    } else if (number < 100) {
+        const tenDigit = Math.floor(number / 10);
+        const unitDigit = number % 10;
+        return `${tens[tenDigit]} ${digits[unitDigit]}`.trim();
+    } else {
+        const hundredDigit = Math.floor(number / 100);
+        const tenRemainder = number % 100;
+        const readableRemainder = (tenRemainder > 0) ? `${toReadable(tenRemainder)}` : '';
+        return `${digits[hundredDigit]} hundred ${readableRemainder}`.trim();
+    }
 }
